054a9bd657bd039eea79af893148f26669756045
Gasg10 <goncalogoncalves@gmail.com>
2024-10-01 16:54:46 +0100

    Initial commit

---
+++ src/Main.kt
@@ -0,0 +1,12 @@
+fun main() {
+    println("Qual o primeiro numero?")
+    var num1 = readln().toInt()
+    println("Qual o segundo numero?")
+    var num2 = readLine().toInt
+    if (num1 > num2){
+        println("O maior numero é o $num1")
+    }
+    else{
+        println("O maior numero é o $num2")
+    }
+}
